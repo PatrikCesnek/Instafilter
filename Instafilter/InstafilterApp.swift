d0bd68c36ca5e03b8cce094a7d3d6f1e49caabfb
PatrikCesnek <pcesnek290@gmail.com>
2020-10-19 13:25:53 +0200

    Innitial commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Patrik Cesnek on 07/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
